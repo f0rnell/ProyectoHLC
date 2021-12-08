8297b8c8c4c383b4952f7a6a5f198555bb06d843
f0rnell <jformar967@g.educaand.es>
2021-12-08 14:09:56 +0100

    Comit desde casa 08/11/2021

--- src/app/detalle/detalle.page.ts
+++ src/app/detalle/detalle.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Jugador } from '../jugador';
+import { FirestoreService } from '../firestore.service';
 
 @Component({
   selector: 'app-detalle',
@@ -9,11 +11,28 @@ import { ActivatedRoute } from '@angular/router';
 export class DetallePage implements OnInit {
 
   id: string = "";
-  constructor(private activatedRoute: ActivatedRoute) { }
+  document: any = {
+    id: "",
+    data: {} as Jugador
+  };
+  constructor(private activatedRoute: ActivatedRoute, private firestoreService: FirestoreService) { }
 
   ngOnInit() {
     this.id = this.activatedRoute.snapshot.paramMap.get('id')
+    this.firestoreService.consultarPorId("jugador", this.id).subscribe((resultado) => {
+      // Preguntar si se hay encontrado un document con ese ID
+      if(resultado.payload.data() != null) {
+        this.document.id = resultado.payload.id
+        this.document.data = resultado.payload.data();
+        // Como ejemplo, mostrar el título de la tarea en consola
+        console.log(this.document.data.nombre);
+      } else {
+        // No se ha encontrado un document con ese ID. Vaciar los datos que hubiera
+        this.document.data = {} as Jugador;
+      } 
+    });
 
   }
+  
 
 }
